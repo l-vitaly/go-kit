280fb4269f0df13823f7ac343a0f18c1280ca55e
Vitaly Lobchuk <vitaly@lobchuk.ru>
2017-06-22 15:14:17 +0300

    refactor(all): some changes

--- transportlayer/transportlayer.go
+++ transportlayer/transportlayer.go
@@ -13,13 +13,32 @@ type OptionFactory interface {
 	CreateOptions(m string) []EndpointOption
 }
 
-type ServerTransportLayer struct {
-	endpoints []Endpoint
+// MakeServerEndpoints
+func MakeServerEndpoints(s interface{}, ef EndpointFactory, of OptionFactory) []Endpoint {
+	var endpoints []Endpoint
+	structMethods(s, func(method string) {
+		e := ef.CreateEndpoint(method)
+		options := of.CreateOptions(method)
+		if e != nil && options != nil {
+			endpoints = append(endpoints, NewEndpoint(method, e, options...))
+		}
+	})
+	return endpoints
 }
 
-// NewServerTransportLayer
-func NewServerTransportLayer(s interface{}, ef EndpointFactory, of OptionFactory) *ServerTransportLayer {
-	tl := &ServerTransportLayer{}
+// MakeClientEndpoints
+func MakeClientEndpoints(s interface{}, of OptionFactory) []Endpoint {
+	var endpoints []Endpoint
+	structMethods(s, func(method string) {
+		options := of.CreateOptions(method)
+		if options != nil {
+			endpoints = append(endpoints, NewEndpoint(method, nil, options...))
+		}
+	})
+	return endpoints
+}
+
+func structMethods(s interface{}, fn func(method string)) {
 	sType := reflect.TypeOf(s)
 	for i := 0; i < sType.NumMethod(); i++ {
 		method := sType.Method(i)
@@ -27,34 +46,6 @@ func NewServerTransportLayer(s interface{}, ef EndpointFactory, of OptionFactory
 		if method.PkgPath != "" {
 			continue
 		}
-		tl.endpoints = append(tl.endpoints, NewEndpoint(method.Name, ef.CreateEndpoint(method.Name), of.CreateOptions(method.Name)...))
+		fn(method.Name)
 	}
-	return tl
-}
-
-func (t *ServerTransportLayer) GetEndpoints() []Endpoint {
-	return t.endpoints
-}
-
-type ClientTransportLayer struct {
-	endpoints []Endpoint
-}
-
-// NewClientTransportLayer
-func NewClientTransportLayer(s interface{}, of OptionFactory) *ClientTransportLayer {
-	tl := &ClientTransportLayer{}
-	sType := reflect.TypeOf(s)
-	for i := 0; i < sType.NumMethod(); i++ {
-		method := sType.Method(i)
-		// Method must be exported.
-		if method.PkgPath != "" {
-			continue
-		}
-		tl.endpoints = append(tl.endpoints, NewEndpoint(method.Name, nil, of.CreateOptions(method.Name)...))
-	}
-	return tl
-}
-
-func (t *ClientTransportLayer) GetEndpoints() []Endpoint {
-	return t.endpoints
 }
